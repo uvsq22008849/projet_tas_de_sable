cb923ab57012955ba4f55a3c86fec7b6addb235a
Pierre RATCLIFFE <pierre.ratcliffe@ens.uvsq.fr>
2022-02-07 15:06:39 +0100

    Etape 8 Doc

---
+++ tas_de_sable.py
@@ -0,0 +1,8 @@
+#########################################
+# groupe MI 1
+# Pierre RATCLIFFE
+# Yassine BAGHDAD-BEY
+# Sephora TAHA
+# Nique PRYSCIA
+# https://github.com/uvsq22008849/projet_tas_de_sable.git
+#########################################
